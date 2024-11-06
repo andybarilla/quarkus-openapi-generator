477d87b3b9697dae2f4d4260d8b7b85d3cf8465b
Andy Barilla <andy.barilla@titlesense.ai>
2024-11-06 09:43:40 -0700

    PR review

--- client/deployment/src/main/java/io/quarkiverse/openapi/generator/deployment/wrapper/QuarkusJavaClientCodegen.java
+++ client/deployment/src/main/java/io/quarkiverse/openapi/generator/deployment/wrapper/QuarkusJavaClientCodegen.java
@@ -70,8 +70,8 @@ public class QuarkusJavaClientCodegen extends JavaClientCodegen {
         supportingFiles.clear();
 
         Boolean enableSecurityGeneration = (Boolean) this.additionalProperties.get("enable-security-generation");
-        Boolean generateApis = (Boolean) this.additionalProperties.getOrDefault("generate-apis", true);
-        Boolean generateModels = (Boolean) this.additionalProperties.getOrDefault("generate-models", true);
+        Boolean generateApis = (Boolean) this.additionalProperties.getOrDefault("generate-apis", Boolean.TRUE);
+        Boolean generateModels = (Boolean) this.additionalProperties.getOrDefault("generate-models", Boolean.TRUE);
 
         if (enableSecurityGeneration == null || enableSecurityGeneration) {
             if (ProcessUtils.hasHttpBasicMethods(this.openAPI) ||
@@ -94,12 +94,12 @@ public class QuarkusJavaClientCodegen extends JavaClientCodegen {
         }
 
         apiTemplateFiles.clear();
-        if (Boolean.TRUE.equals(generateApis)) {
+        if (generateApis) {
             apiTemplateFiles.put("api.qute", ".java");
         }
 
         modelTemplateFiles.clear();
-        if (Boolean.TRUE.equals(generateModels)) {
+        if (generateModels) {
             modelTemplateFiles.put("model.qute", ".java");
         }
     }
